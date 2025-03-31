f2a21eafa2881a1800dfc8c4f6a6dcc454b428d2
nahlasa-a <sayedahmad.nahla@gmail.com>
2025-03-30 20:46:40 -0400

    keyboard events in javascript

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ hoverButton.addEventListener('mouseleave', () => {
     mouseMessage.textContent = 'Mouse is outside the button!';
     mouseMessage.style.color = 'beige';
 });
+
+const keyboardInput = document.getElementById('keyboard-input');
+const keyboardMessage = document.getElementById('keyboard-message');
+keyboardInput.addEventListener('keyup', (event) => {
+    let key = event.key;
+    if (keyPressed === '') {
+        keyPressed = 'Space';
+    } else if (keyPressed === 'Enter') {
+        keyPressed = 'Enter key';
+    }
+
+    keyboardMessage.textContent = `You pressed: ${keyPressed}`;
+    keyboardMessage.style.color = 'beige';
+    setTimeout(() => {
+        keyboardMessage.style.color = 'light brown';
+    }
+    , 2000);
+});
