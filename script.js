77011bd9444c0fc2c0f93edf190805f877752731
nahlasa-a <sayedahmad.nahla@gmail.com>
2025-03-30 22:15:37 -0400

    edits, final touches

--- script.js
+++ script.js
@@ -12,19 +12,19 @@ hoverButton.addEventListener('mouseleave', () => {
 });
 
 const keyboardInput = document.getElementById('keyboard-input');
-const keyboardMessage = document.getElementById('keyboard-message');
+const keyMessage = document.getElementById('key-message');
 keyboardInput.addEventListener('keyup', (event) => {
     let key = event.key;
-    if (keyPressed === '') {
-        keyPressed = 'Space';
-    } else if (keyPressed === 'Enter') {
-        keyPressed = 'Enter key';
+    if (key === '') {
+        key = 'Space';
+    } else if (key === 'Enter') { 
+        key = 'Enter key';
     }
 
-    keyboardMessage.textContent = `You pressed: ${keyPressed}`;
-    keyboardMessage.style.color = 'beige';
+    keyMessage.textContent = `You pressed: ${keyPressed}`;
+    keyMessage.style.color = 'beige';
     setTimeout(() => {
-        keyboardMessage.style.color = 'light brown';
+        keyboardMessage.style.color = 'brown';
     }
     , 2000);
 });
@@ -35,7 +35,7 @@ const formMessage = document.getElementById('form-message');
 demoForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    const formInput = document.getElementById('form-input');
+    const formInput = document.getElementById('name');
     const inputValue = formInput.value.trim();
     if (inputValue) {
         formMessage.textContent = `Form submitted.: ${inputValue}`;
@@ -76,26 +76,16 @@ const delegationMessage = document.getElementById('delegation-message');
 
 buttonContainer.addEventListener('click', (event) => { 
     if (event.target.tagName === 'BUTTON') { 
-
         const buttonText = event.target.textContent;
-
         delegationMessage.textContent = `Clicked: ${buttonText}`; 
         delegationMessage.style.backgroundColor = '#e8f4fc'; 
 
-        event.target.style.backgroundColor = '#darkorange'; 
+        event.target.style.backgroundColor = 'gray';
+        event.target.style.color = 'white'; 
 
         setTimeout(() => { 
-            event.target.style.backgroundColor = '#darkred'; 
-            delegationMessage.style.backgroundColor = '#lightred'; 
+            event.target.style.backgroundColor = 'purple'; 
+            delegationMessage.style.backgroundColor = 'orange'; 
         }, 1000); 
     } 
-}); 
-
-const buttonData = ['Button 1', 'Button 2', 'Button 3']; 
-
-buttonData.forEach(text => { 
-    const newButton = document.createElement('button'); 
-    newButton.className = 'delegated-btn'; 
-    newButton.textContent = text; 
-    buttonContainer.appendChild(newButton); 
 }); 
